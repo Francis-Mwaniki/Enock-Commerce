95a852dbee0fa51491bb65787901f6c73eed7401
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-06-15 10:37:51 +0300

    Enocks Ecommerce

---
+++ commerce_store/urls.py
@@ -0,0 +1,8 @@
+from  django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("About", views.About, name="About"),
+    path("Contact", views.Contact, name="Contact")
+]
